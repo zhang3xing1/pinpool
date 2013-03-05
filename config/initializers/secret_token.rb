1e8526044629802426af3889467a28597e5ef83e
Frday <zhang3xing1@gmail.com>
2013-03-06 00:57:06 +0800

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pinpool::Application.config.secret_token = '3057b5fc40dccc694d234ffd16052695d6b97b422eee0b863cfe6d7cdffb52068c5342ab954bbe6d25e5ead77e8576e3e4c3f8c40cb9eaabdc4529d0d3d4c195'
